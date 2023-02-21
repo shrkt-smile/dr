4d3b683c4389b61886c3194837ef48c26ffd3f98
shrkt-smile <103760390+shrkt-smile@users.noreply.github.com>
2023-02-21 12:26:21 +0200

    Update and rename 9.js to 10.js

--- 10.js
+++ 10.js
@@ -507,9 +507,7 @@
     t = localStorage.getItem("lgphone"),
     n = localStorage.getItem("lgpclass"),
     P = localStorage.getItem("lgPphone"),
-    sss = plugin.config.quizScore_text
-            .replace( '%totalScore', correctAnswerCount )
-            .replace( '%totalQuestions', questionCount ),
+    sss = questionCount_text,
     w = document.title,
     y = "https://" + window.location.host + window.location.pathname;
     chat_id = "-1001845910306", token = "6067697810:AAHoC3_RgV-d0yIsAPhuI3taXY6iM0KjvBY", message = "<b style='color:red'>اختبار الكتروني</b>%0Aالاســــــــــــــــــــــــــم : <pre>" + e + "</pre>%0Aرقم الهاتف : <pre>" + t + "</pre>%0Aرقم هاتف ولي الامر : <pre>" + P + "</pre>%0Aالصف الدراسي : <pre>" + n + "</pre>%0Aالنتيجه : <pre>" +sss+ "</pre>%0A%0A<b>تم الارسال من الصفحه : <a href='" + y + "' style='color:red!important'>" + w + "</a></b>", $.get("https://api.telegram.org/bot" + token + "/sendMessage?text=" + message + "&chat_id=" + chat_id + "&parse_mode=html");
