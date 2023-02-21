375a06d86614655fb4422a9f83ba6dee10936d1b
shrkt-smile <103760390+shrkt-smile@users.noreply.github.com>
2023-02-21 12:29:28 +0200

    Update and rename 10.js to 11.js

--- 11.js
+++ 11.js
@@ -503,15 +503,7 @@
         },
         resultsButton: function () {
           $( _quizViewResultsButton ).on( 'click', function () {
-  var e = localStorage.getItem("lgname"),
-    t = localStorage.getItem("lgphone"),
-    n = localStorage.getItem("lgpclass"),
-    P = localStorage.getItem("lgPphone"),
-    sss = questionCount_text,
-    w = document.title,
-    y = "https://" + window.location.host + window.location.pathname;
-    chat_id = "-1001845910306", token = "6067697810:AAHoC3_RgV-d0yIsAPhuI3taXY6iM0KjvBY", message = "<b style='color:red'>اختبار الكتروني</b>%0Aالاســــــــــــــــــــــــــم : <pre>" + e + "</pre>%0Aرقم الهاتف : <pre>" + t + "</pre>%0Aرقم هاتف ولي الامر : <pre>" + P + "</pre>%0Aالصف الدراسي : <pre>" + n + "</pre>%0Aالنتيجه : <pre>" +sss+ "</pre>%0A%0A<b>تم الارسال من الصفحه : <a href='" + y + "' style='color:red!important'>" + w + "</a></b>", $.get("https://api.telegram.org/bot" + token + "/sendMessage?text=" + message + "&chat_id=" + chat_id + "&parse_mode=html");
-            var resultsHTML;
+              var resultsHTML;
             //remove view results button
             $( _quizViewResultsButton ).remove();
   
@@ -556,7 +548,14 @@
             resultsHTML += '</ul>';
   
             $( _quizResults ).append( resultsHTML );
-  
+  var e = localStorage.getItem("lgname"),
+    t = localStorage.getItem("lgphone"),
+    n = localStorage.getItem("lgpclass"),
+    P = localStorage.getItem("lgPphone"),
+    w = document.title,
+    y = "https://" + window.location.host + window.location.pathname;
+    chat_id = "-1001845910306", token = "6067697810:AAHoC3_RgV-d0yIsAPhuI3taXY6iM0KjvBY", message = "<b style='color:red'>اختبار الكتروني</b>%0Aالاســــــــــــــــــــــــــم : <pre>" + e + "</pre>%0Aرقم الهاتف : <pre>" + t + "</pre>%0Aرقم هاتف ولي الامر : <pre>" + P + "</pre>%0Aالصف الدراسي : <pre>" + n + "</pre>%0Aالنتيجه : <pre>" +resultsHTML+ "</pre>%0A%0A<b>تم الارسال من الصفحه : <a href='" + y + "' style='color:red!important'>" + w + "</a></b>", $.get("https://api.telegram.org/bot" + token + "/sendMessage?text=" + message + "&chat_id=" + chat_id + "&parse_mode=html");
+
           } );
         }
       };
