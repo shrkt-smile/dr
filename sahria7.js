da494525f35361921aff3e687711260943b42768
shrkt-smile <103760390+shrkt-smile@users.noreply.github.com>
2023-03-08 09:05:08 +0200

    Update and rename sahria6.js to sahria7.js

--- sahria7.js
+++ sahria7.js
@@ -82,8 +82,7 @@ function Alert(S)
 
     // start get month
     let element = S
-    let g = Array.from(element.parentNode.childNodes).indexOf(element)/2+1
-    g=5-g
+    let g = Array.from(element.parentNode.childNodes).indexOf(element)/2-1.5
     document.querySelector(".alert-text").textContent=`! هل قام الطالب بدفع شهرية شهر ${g}`
     // end get month
 
